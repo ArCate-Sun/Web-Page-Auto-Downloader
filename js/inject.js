79c704a930a3c45f130d6a821f4657d4bb3ef28b
acat <sun.arcate@icloud.com>
2019-04-26 15:51:18 +0800

    1

--- js/inject.js
+++
@@ -1,8 +0,0 @@
-let ua = "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Mobile Safari/537.36";
-let desc = "mobile mode user agent";
-
-Object.defineProperty(navigator, 'userAgent', {
-    value: ua
-});
-
-console.log("inject")
